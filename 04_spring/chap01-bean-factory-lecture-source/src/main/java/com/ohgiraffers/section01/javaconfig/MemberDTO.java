80eefd41bd8a64ae3bf7be9a73a21c466c5c23db
B <mox9nox@gmail.com>
2024-02-08 15:36:27 +0900

    bean factory 예제 작성 완료

---
+++ 04_spring/chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/MemberDTO.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section01.javaconfig;
+
+import lombok.*;
+
+// annotation 붙이면 굳이 밑에 다 안 써도 됨
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class MemberDTO {
+    private int sequence;
+    private String id;
+    private String pwd;
+    private String name;
+}
